53f88a7a6d3f0441cde4feaec387ef78908e586a
Phill Conrad <pconrad@cs.ucsb.edu>
2016-08-22 12:11:26 -0700

    starter code ready to go

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # lab10_startercode
-SPIS 2016 lab10 starter code, uses car data from https://think.cs.vt.edu/corgis/
+
+
+SPIS 2016 lab10 starter code, uses car data from 
+
+For lab instructions, visit: http://ucsd-cse-spis-2016.github.io/lab/lab10/
+
+# About cars.py and cars.db
+
+These are from https://think.cs.vt.edu/corgis/
+
+https://think.cs.vt.edu/corgis//python/cars/cars.html for helpful
+hints on working with this data.
+
+
+
---
+++ car_analysis.py
@@ -0,0 +1,30 @@
+# import car data from https://think.cs.vt.edu/corgis/
+import cars
+
+def yearOfCar(car):
+    return "stub"
+
+
+def horsepower(cardata):
+    return "stub"  # replace this with actual code
+
+# You will end up adding additional functions here
+
+
+
+if __name__=="__main__":
+   print """
+Try:
+
+   cardata = cars.get_cars()
+
+   len(cardata)
+   cardata[0]
+   cardata[0].keys()
+   cardata[0]['some_key']
+
+   """
+
+   # You might add additional code below this comment if you want it to run
+   # when you run the file.  Be sure to indent three spaces, just like these comments.
+   
