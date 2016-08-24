7529b22dff1672228250630c86b0783140840604
AndrewEToh <aetoh@ucsd.edu>
2016-08-23 22:20:25 -0700

    Complete

--- car_analysis.py
+++ car_analysis.py
@@ -1,16 +1,49 @@
 # import car data from https://think.cs.vt.edu/corgis/
 import cars
 
+cardata = cars.get_cars()
+
 def yearOfCar(car):
-    return "stub"
+    return car['Identification']['Year']
 
 
 def horsepower(cardata):
-    return "stub"  # replace this with actual code
+    hp_list = []
+    for i in range(len(cardata)):
+        hp_list.append(cardata[i]['Engine Information']['Engine Statistics']['Horsepower'])
+    return hp_list
 
-# You will end up adding additional functions here
+hp_list = horsepower(cardata)
 
+import matplotlib.pyplot as plt
 
+from collections import defaultdict
+
+def histogramify(data,bins):
+	M = max(data)
+	m = min(data)
+	interval = (M-m)/bins
+	hist = defaultdict(int)
+	for d in data:
+		for i in range(bins):
+			
+			#This line originally said: if d>m+i*interval:
+			if d>m+i*interval and d<=m+(i+1)*interval:
+				f=m+i*interval
+		hist[f]+=1
+	return hist
+
+hist_HP = histogramify(hp_list,25)
+
+def MPG(cardata):
+    MPG_list = []
+    for i in range(len(cardata)):
+        MPG_list.append(cardata[i]['Fuel Information']['Highway mpg'])
+    return MPG_list
+
+MPG_list = MPG(cardata)
+
+hist_MPG = histogramify(MPG_list,25)
 
 if __name__=="__main__":
    print """
